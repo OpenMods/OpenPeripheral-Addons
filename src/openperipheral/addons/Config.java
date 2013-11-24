1ec8a84d60293f807f997ff19bb43f8d2798d5f9
Mikee Franklin <mikeefranklin@gmail.com>
2013-11-24 13:31:04 +0000

    fixed addons to look at renamed config stuff

--- src/openperipheral/addons/Config.java
+++ src/openperipheral/addons/Config.java
@@ -5,10 +5,8 @@ import java.util.List;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.common.Configuration;
-import openmods.interfaces.BlockId;
-import openmods.utils.BlockUtils;
-import openmods.utils.ConfigUtils;
-import openmods.utils.ItemUtils;
+import openmods.config.BlockId;
+import openmods.config.ConfigProcessing;
 import openperipheral.addons.common.block.BlockGlassesBridge;
 
 public class Config {
@@ -17,17 +15,17 @@ public class Config {
 	public static int blockGlassesBridgeId = 3000;
 	
 	static void readConfig(Configuration configFile) {
-		ConfigUtils.processAnnotations(configFile, Config.class);
+		ConfigProcessing.processAnnotations(configFile, Config.class);
 	}
 
 	public static void register() {
 		@SuppressWarnings("unchecked")
 		final List<IRecipe> recipeList = CraftingManager.getInstance().getRecipeList();
 
-		if (BlockUtils.canRegisterBlock(blockGlassesBridgeId)) {
+		if (ConfigProcessing.canRegisterBlock(blockGlassesBridgeId)) {
 			OpenPeripheralAddons.Blocks.glassesBridge = new BlockGlassesBridge();
 		}
 		
-		ItemUtils.registerItems(OpenPeripheralAddons.Items.class, "openperipheraladdons");
+		ConfigProcessing.registerItems(OpenPeripheralAddons.Items.class, "openperipheraladdons");
 	}
 }
