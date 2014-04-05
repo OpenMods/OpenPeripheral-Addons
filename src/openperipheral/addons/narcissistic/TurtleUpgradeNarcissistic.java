eefbaa3ec6025221e3660050d00275c2e79b227d
Bartek Bok <bartek.bok@gmail.com>
2014-04-05 13:30:02 +0200

    Fix narcissistic turtle

--- src/openperipheral/addons/narcissistic/TurtleUpgradeNarcissistic.java
+++ src/openperipheral/addons/narcissistic/TurtleUpgradeNarcissistic.java
@@ -33,7 +33,7 @@ public class TurtleUpgradeNarcissistic implements ITurtleUpgrade {
 
 	@Override
 	public IPeripheral createPeripheral(ITurtleAccess turtle, TurtleSide side) {
-		return PeripheralHandlers.createAdaptedPeripheral(turtle);
+		return PeripheralHandlers.createAdaptedPeripheral(turtle.getInventory());
 	}
 
 	@Override
