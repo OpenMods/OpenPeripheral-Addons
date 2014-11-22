ac2a72e317d87bb2e1f249f9a584da741626fbbd
Bartek Bok <bartek.bok@gmail.com>
2014-11-22 15:55:02 +0100

    Align to changes in lib

--- src/main/java/openperipheral/addons/proxy/ClientProxy.java
+++ src/main/java/openperipheral/addons/proxy/ClientProxy.java
@@ -3,9 +3,11 @@ package openperipheral.addons.proxy;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
 import openmods.api.IProxy;
+import openmods.renderer.BlockRenderingHandler;
 import openperipheral.addons.OpenPeripheralAddons;
-import openperipheral.addons.client.BlockRenderingHandler;
+import openperipheral.addons.OpenPeripheralAddons.Blocks;
 import openperipheral.addons.glasses.TerminalManagerClient;
+import openperipheral.addons.pim.BlockPIMRenderer;
 import openperipheral.addons.sensors.TileEntitySensor;
 import openperipheral.addons.sensors.TileEntitySensorRenderer;
 import cpw.mods.fml.client.registry.ClientRegistry;
@@ -39,7 +41,11 @@ public class ClientProxy implements IProxy {
 	@Override
 	public void registerRenderInformation() {
 		OpenPeripheralAddons.renderId = RenderingRegistry.getNextAvailableRenderId();
-		RenderingRegistry.registerBlockHandler(new BlockRenderingHandler());
+		final BlockRenderingHandler blockRenderingHandler = new BlockRenderingHandler(OpenPeripheralAddons.renderId);
+		RenderingRegistry.registerBlockHandler(blockRenderingHandler);
+
+		blockRenderingHandler.addRenderer(Blocks.pim, new BlockPIMRenderer());
+
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntitySensor.class, new TileEntitySensorRenderer());
 	}
 }
