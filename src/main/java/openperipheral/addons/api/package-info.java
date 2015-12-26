3f0fddc6d46664b71c4f9cac4c95981a795318bd
Bartek Bok <bartek.bok@gmail.com>
2015-12-26 17:08:46 +0100

    Fill API version in gradle

--- src/main/java/openperipheral/addons/api/package-info.java
+++ src/main/java/openperipheral/addons/api/package-info.java
@@ -1,4 +1,4 @@
-@API(apiVersion = "1.0", owner = "OpenPeripheralAddons", provides = "OpenPeripheralAddonsApi")
+@API(apiVersion = "$API-VERSION$", owner = "OpenPeripheralAddons", provides = "OpenPeripheralAddonsApi")
 package openperipheral.addons.api;
 
 import cpw.mods.fml.common.API;
