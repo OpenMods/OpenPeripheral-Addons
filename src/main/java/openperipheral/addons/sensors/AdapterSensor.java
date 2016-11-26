5516a45c0a7de6b97790ce5559c6ea0a8c80ccc1
James Smith <james@nullious.net>
2016-11-26 14:57:45 -0600

    Add new lua-accessible function Sensor peripheral to allow sonic scanning of a single location. Add map-color information about blocks to the data returned by sonic scans.

--- src/main/java/openperipheral/addons/sensors/AdapterSensor.java
+++ src/main/java/openperipheral/addons/sensors/AdapterSensor.java
@@ -4,6 +4,8 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.mojang.authlib.GameProfile;
+
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -44,6 +46,85 @@ public class AdapterSensor implements IPeripheralAdapter {
 
 		private SupportedEntityTypes(Class<? extends Entity> cls) {
 			this.cls = cls;
+		}
+	}
+
+	private static enum ComputerColors {
+		WHITE     (0xF0F0F0, "0"),
+		ORANGE    (0xF2B233, "1"),
+		MAGENTA   (0xE57FD8, "2"),
+		LIGHT_BLUE(0x99B2F2, "3"),
+		YELLOW    (0xDEDE6C, "4"),
+		LIME      (0x7FCC19, "5"),
+		PINK      (0xF2B2CC, "6"),
+		GRAY      (0x4C4C4C, "7"),
+		LIGHT_GRAY(0x999999, "8"),
+		CYAN      (0x4C99B2, "9"),
+		PURPLE    (0xB266E5, "a"),
+		BLUE      (0x3366CC, "b"),
+		BROWN     (0x7F664C, "c"),
+		GREEN     (0x57A64E, "d"),
+		RED       (0xCC4C4C, "e"),
+		BLACK     (0x000000, "f");
+
+		public final int R;
+		public final int G;
+		public final int B;
+		public final String code;
+
+		// LRU cache with a maximum of 1000 entries
+		private static final LinkedHashMap<Integer, ComputerColors> LOOKUP_CACHE = new LinkedHashMap<Integer, ComputerColors>(16, 0.75f, true) {
+		  protected boolean removeEldestEntry(Map.Entry<Integer, ComputerColors> eldest) {
+		    return size() > 1000;
+		  }
+		};
+
+		ComputerColors(int rgb, String code) {
+			this.R = (rgb >>> 16) & 0xFF;
+			this.G = (rgb >>> 8) & 0xFF;
+			this.B = rgb & 0xFF;
+			this.code = code;
+		}
+
+		// Formula taken from http://www.compuphase.com/cmetric.htm
+		private int distanceSquared(int cr, int cg, int cb) {
+			int r_mean = (this.R + cr) >>> 1;
+			int r_delta = this.R - cr;
+			int g_delta = this.G - cg;
+			int b_delta = this.B - cb;
+
+			int rd2 = r_delta*r_delta;
+			int gd2 = g_delta*g_delta;
+			int bd2 = b_delta*b_delta;
+
+			return Math.abs((((512+r_mean)*rd2)>>8)
+					   + (gd2 << 2)
+					   + (((767-r_mean)*bd2)>>8));
+		}
+
+		public static ComputerColors getClosestColor(int rgb) {
+			ComputerColors cached = ComputerColors.LOOKUP_CACHE.get(rgb);
+			if (cached != null) {
+				return cached;
+			}
+
+			int cr = (rgb >>> 16) & 0xFF;
+			int cg = (rgb >>> 8) & 0xFF;
+			int cb = rgb & 0xFF;
+
+			ComputerColors lowest = null;
+			int lowest_score = Integer.MAX_VALUE;
+
+			for (ComputerColors cc : ComputerColors.values()) {
+				int cc_score = cc.distanceSquared(cr, cg, cb);
+				if (lowest == null || cc_score < lowest_score) {
+					lowest = cc;
+					lowest_score = cc_score;
+				}
+			}
+
+			ComputerColors.LOOKUP_CACHE.put(rgb, lowest);
+			return lowest;
 		}
 	}
 
@@ -160,7 +241,7 @@ public class AdapterSensor implements IPeripheralAdapter {
 		return getPlayerInfo(env, uuid);
 	}
 
-	@ScriptCallable(returnTypes = ReturnType.TABLE, description = "Get a table of information about the surrounding area. Includes whether each block is UNKNOWN, AIR, LIQUID or SOLID")
+	@ScriptCallable(returnTypes = ReturnType.TABLE, description = "Get a table of information about the surrounding area. Includes map color and whether each block is UNKNOWN, AIR, LIQUID, or SOLID.")
 	public Map<Integer, Map<String, Object>> sonicScan(ISensorEnvironment env) {
 
 		int range = 1 + env.getSensorRange() / 2;
@@ -191,16 +272,73 @@ public class AdapterSensor implements IPeripheralAdapter {
 					else if (block.getMaterial().isSolid()) type = "SOLID";
 					else type = "UNKNOWN";
 
+					final String color;
+					if (block == null) {
+						color = "f"; // Default black -- same as air.
+					} else {
+						int rgb = block.getMapColor(block.getDamageValue(world, bx, by, bz)).colorValue;
+						color = ComputerColors.getClosestColor(rgb).code;
+					}
+
 					Map<String, Object> tmp = Maps.newHashMap();
 					tmp.put("x", x);
 					tmp.put("y", y);
 					tmp.put("z", z);
 					tmp.put("type", type);
+					tmp.put("color", color);
 					results.put(++i, tmp);
 
 				}
 			}
 		}
 		return results;
+	}
+
+	@ScriptCallable(returnTypes = ReturnType.TABLE, description = "Get a table of information about a single block in the surrounding area. Includes map color and whether each block is UNKNOWN, AIR, LIQUID, or SOLID.")
+	public Map<String, Object> sonicScanTarget(ISensorEnvironment env,
+			@Arg(name = "xOffset", description = "The target's offset from the sensor on the X-Axis.") int xOff,
+			@Arg(name = "yOffset", description = "The target's offset from the sensor on the Y-Axis.") int yOff,
+			@Arg(name = "zOffset", description = "The target's offset from the sensor on the Z-Axis.") int zOff) {
+
+		int range = 1 + env.getSensorRange() / 2;
+		World world = env.getWorld();
+		Vec3 sensorPos = env.getLocation();
+		int sx = MathHelper.floor_double(sensorPos.xCoord);
+		int sy = MathHelper.floor_double(sensorPos.yCoord);
+		int sz = MathHelper.floor_double(sensorPos.zCoord);
+
+		final int rangeSq = range * range;
+
+		final int bx = sx + xOff;
+		final int by = sy + yOff;
+		final int bz = sz + zOff;
+		if (!world.blockExists(bx, by, bz)) return null;
+
+		final int distSq = xOff * xOff + yOff * yOff + zOff * zOff;
+		if (distSq == 0 || distSq > rangeSq) return null;
+		Block block = world.getBlock(bx, by, bz);
+
+		String type;
+		if (block == null || world.isAirBlock(bx, by, bz)) type = "AIR";
+		else if (block.getMaterial().isLiquid()) type = "LIQUID";
+		else if (block.getMaterial().isSolid()) type = "SOLID";
+		else type = "UNKNOWN";
+
+		final String color;
+		if (block == null) {
+			color = "f"; // Default black -- same as air.
+		} else {
+			int rgb = block.getMapColor(block.getDamageValue(world, bx, by, bz)).colorValue;
+			color = ComputerColors.getClosestColor(rgb).code;
+		}
+
+		Map<String, Object> tmp = Maps.newHashMap();
+		tmp.put("x", xOff);
+		tmp.put("y", yOff);
+		tmp.put("z", zOff);
+		tmp.put("type", type);
+		tmp.put("color", color);
+
+		return tmp;
 	}
 }
