d35d47188b5e80fc6ca804306f4e43aa236a6d85
Bartek Bok <bartek.bok@gmail.com>
2016-11-27 20:11:49 +0100

    Remove duplications

--- src/main/java/openperipheral/addons/sensors/AdapterSensor.java
+++ src/main/java/openperipheral/addons/sensors/AdapterSensor.java
@@ -4,7 +4,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.mojang.authlib.GameProfile;
-
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,9 +20,9 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
-import openmods.utils.WorldUtils;
 import openmods.utils.ColorUtils;
 import openmods.utils.ColorUtils.RGB;
+import openmods.utils.WorldUtils;
 import openperipheral.addons.OpcAccess;
 import openperipheral.api.adapter.IPeripheralAdapter;
 import openperipheral.api.adapter.method.Arg;
@@ -54,9 +53,10 @@ public class AdapterSensor implements IPeripheralAdapter {
 	// LRU cache with a maximum of 1000 entries; maps packed-RGB int -> color-bitmask int
 	@SuppressWarnings("serial")
 	private static final LinkedHashMap<Integer, Integer> COLOR_CACHE = new LinkedHashMap<Integer, Integer>(16, 0.75f, true) {
-	  protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-	    return size() > 1000;
-	  }
+		@Override
+		protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+			return size() > 1000;
+		}
 	};
 
 	@Override
@@ -115,40 +115,51 @@ public class AdapterSensor implements IPeripheralAdapter {
 		return OpcAccess.entityMetaBuilder.createProxy(mob, sensorPos);
 	}
 
-	private static String getBlockType(ISensorEnvironment sensor, int xPos, int yPos, int zPos) {
-		World world = sensor.getWorld();
+	private static boolean checkRange(int x, int y, int z, int rangeSq) {
+		final int distSq = x * x + y * y + z * z;
+		return (distSq == 0 || distSq > rangeSq);
+	}
 
-		if (!world.blockExists(xPos, yPos, zPos)) return null;
-
-		Block block = world.getBlock(xPos, yPos, zPos);
-
+	private static String getBlockType(World world, Block block, int xPos, int yPos, int zPos) {
 		if (block == null || world.isAirBlock(xPos, yPos, zPos)) return "AIR";
 		else if (block.getMaterial().isLiquid()) return "LIQUID";
 		else if (block.getMaterial().isSolid()) return "SOLID";
 		else return "UNKNOWN";
 	}
 
-	private static int getBlockColorBitmask(ISensorEnvironment sensor, int xPos, int yPos, int zPos) {
-		World world = sensor.getWorld();
-
-		if (!world.blockExists(xPos, yPos, zPos)) return 0;
-
-		Block block = world.getBlock(xPos, yPos, zPos);
-
+	private static int getBlockColorBitmask(World world, Block block, int xPos, int yPos, int zPos) {
 		if (block == null) {
 			return ColorUtils.ColorMeta.BLACK.bitmask; // Default black -- same as air.
 		} else {
 			int color = block.getMapColor(block.getDamageValue(world, xPos, yPos, zPos)).colorValue;
 			Integer cached = COLOR_CACHE.get(color);
-			if (cached != null) {
-				return cached;
-			}
+			if (cached != null) return cached;
 
 			RGB rgb = new RGB(color);
 			Integer nearestColorBitmask = ColorUtils.findNearestColor(rgb, 255).bitmask;
 			COLOR_CACHE.put(color, nearestColorBitmask);
 			return nearestColorBitmask;
 		}
+	}
+
+	private static Map<String, Object> describeBlock(World world, int sx, int sy, int sz, int dx, int dy, int dz) {
+		final int bx = sx + dx;
+		final int by = sy + dy;
+		final int bz = sz + dz;
+
+		if (!world.blockExists(bx, by, bz)) return null;
+
+		final Block block = world.getBlock(bx, by, bz);
+		final String type = getBlockType(world, block, bx, by, bz);
+		final int color = getBlockColorBitmask(world, block, bx, by, bz);
+
+		Map<String, Object> tmp = Maps.newHashMap();
+		tmp.put("x", dx);
+		tmp.put("y", dy);
+		tmp.put("z", dz);
+		tmp.put("type", type);
+		tmp.put("color", color);
+		return tmp;
 	}
 
 	@ScriptCallable(returnTypes = ReturnType.OBJECT, description = "Get the ids of all the mobs in range. Deprecated, please use getEntityIds('mob')")
@@ -209,75 +220,47 @@ public class AdapterSensor implements IPeripheralAdapter {
 	}
 
 	@ScriptCallable(returnTypes = ReturnType.TABLE, description = "Get a table of information about the surrounding area. Includes map color and whether each block is UNKNOWN, AIR, LIQUID, or SOLID.")
-	public Map<Integer, Map<String, Object>> sonicScan(ISensorEnvironment env) {
-
+	public List<Map<String, Object>> sonicScan(ISensorEnvironment env) {
 		int range = 1 + env.getSensorRange() / 2;
-		Map<Integer, Map<String, Object>> results = Maps.newHashMap();
+		List<Map<String, Object>> results = Lists.newArrayList();
 		Vec3 sensorPos = env.getLocation();
 		int sx = MathHelper.floor_double(sensorPos.xCoord);
 		int sy = MathHelper.floor_double(sensorPos.yCoord);
 		int sz = MathHelper.floor_double(sensorPos.zCoord);
 
+		final World world = env.getWorld();
+
 		final int rangeSq = range * range;
-		int i = 0;
-		for (int x = -range; x <= range; x++) {
-			for (int y = -range; y <= range; y++) {
-				for (int z = -range; z <= range; z++) {
-					final int bx = sx + x;
-					final int by = sy + y;
-					final int bz = sz + z;
 
-					final int distSq = x * x + y * y + z * z;
-					if (distSq == 0 || distSq > rangeSq) continue;
-
-					final String type = getBlockType(env, bx, by, bz);
-					if (type == null) continue;
-
-					final int color = getBlockColorBitmask(env, bx, by, bz);
-
-					Map<String, Object> tmp = Maps.newHashMap();
-					tmp.put("x", x);
-					tmp.put("y", y);
-					tmp.put("z", z);
-					tmp.put("type", type);
-					tmp.put("color", color);
-					results.put(++i, tmp);
-
+		for (int dx = -range; dx <= range; dx++) {
+			for (int dy = -range; dy <= range; dy++) {
+				for (int dz = -range; dz <= range; dz++) {
+					if (checkRange(dx, dy, dz, rangeSq)) {
+						Map<String, Object> result = describeBlock(world, sx, sy, sz, dx, dy, dz);
+						if (result != null) results.add(result);
+					}
 				}
 			}
 		}
+
 		return results;
 	}
 
 	@ScriptCallable(returnTypes = ReturnType.TABLE, description = "Get a table of information about a single block in the surrounding area. Includes map color and whether each block is UNKNOWN, AIR, LIQUID, or SOLID.")
 	public Map<String, Object> sonicScanTarget(ISensorEnvironment env,
-			@Arg(name = "xOffset", description = "The target's offset from the sensor on the X-Axis.") int xOff,
-			@Arg(name = "yOffset", description = "The target's offset from the sensor on the Y-Axis.") int yOff,
-			@Arg(name = "zOffset", description = "The target's offset from the sensor on the Z-Axis.") int zOff) {
-
+			@Arg(name = "xOffset", description = "The target's offset from the sensor on the X-Axis.") int dx,
+			@Arg(name = "yOffset", description = "The target's offset from the sensor on the Y-Axis.") int dy,
+			@Arg(name = "zOffset", description = "The target's offset from the sensor on the Z-Axis.") int dz) {
 		int range = 1 + env.getSensorRange() / 2;
+		int rangeSq = range * range;
+		if (checkRange(dx, dy, dz, rangeSq)) return null;
+
 		Vec3 sensorPos = env.getLocation();
-		final int rangeSq = range * range;
+		int sx = MathHelper.floor_double(sensorPos.xCoord);
+		int sy = MathHelper.floor_double(sensorPos.yCoord);
+		int sz = MathHelper.floor_double(sensorPos.zCoord);
 
-		final int distSq = xOff * xOff + yOff * yOff + zOff * zOff;
-		if (distSq == 0 || distSq > rangeSq) return null;
-
-		final int bx = MathHelper.floor_double(sensorPos.xCoord) + xOff;
-		final int by = MathHelper.floor_double(sensorPos.yCoord) + yOff;
-		final int bz = MathHelper.floor_double(sensorPos.zCoord) + zOff;
-
-		final String type = getBlockType(env, bx, by, bz);
-		if (type == null) return null;
-
-		final int color = getBlockColorBitmask(env, bx, by, bz);
-
-		Map<String, Object> tmp = Maps.newHashMap();
-		tmp.put("x", xOff);
-		tmp.put("y", yOff);
-		tmp.put("z", zOff);
-		tmp.put("type", type);
-		tmp.put("color", color);
-
-		return tmp;
+		return describeBlock(env.getWorld(), sx, sy, sz, dx, dy, dz);
 	}
+
 }
